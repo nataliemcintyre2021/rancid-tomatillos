ec4fec3a56a4d4a512374c2b680609e41149ba92
Matt Roden <mattroden92@gmail.com>
2021-08-25 17:07:32 -0600

    Render all movie posters to the screen

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import './App.css';
 import movieData from './mockData'
-import Row from './Row'
+import Posters from './Posters'
 
 function App() {
   //console.log(movieData.movies[0], 'movieData<><><>')
+
   return (
     <div className="App">
-      <Row title='All Movies' movieData={movieData.movies} />
-      <Row title='More Movies' movieData={movieData.movies} />
+      <Posters title='All Movies' movieData={movieData.movies} />
     </div>
   );
 }
