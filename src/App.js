b0c83c86f5a2e47117086643dced2c4a675d1593
Matt Roden <mattroden92@gmail.com>
2021-08-25 15:18:29 -0600

    Create initial Row component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import './App.css';
 import movieData from './mockData'
+import Row from './Row'
 
 function App() {
+  //console.log(movieData.movies[0], 'movieData<><><>')
   return (
     <div className="App">
-
+      <Row />
     </div>
   );
 }
---
+++ src/Row.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Row() {
+  return (
+    <h1>Row</h1>
+  )
+}
+
+export default Row
