dd22ad04c8e0b518e6c9eca298f3db377a9fa456
Natalie McIntyre <nataliemcintyre2021@gmail.com>
2021-08-26 16:33:16 -0700

    Create extended view component

---
+++ src/ExtendedView.js
@@ -0,0 +1,11 @@
+import './ExtendedView.css'
+
+function ExtendedView() {
+  return(
+    <div className="extended-view hidden">
+    Extended View
+    </div>
+  )
+}
+
+export default ExtendedView
