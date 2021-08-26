911cbd392f2ecf64d8bac5242521818c555193b9
Natalie McIntyre <nataliemcintyre2021@gmail.com>
2021-08-26 16:09:50 -0700

    Create header and footer elements

--- src/App.js
+++ src/App.js
@@ -2,15 +2,26 @@ import React from 'react'
 import './App.css';
 import movieData from './mockData'
 import Posters from './Posters'
+import Header from './Header'
+import Footer from './Footer'
 
 function App() {
   //console.log(movieData.movies[0], 'movieData<><><>')
 
   return (
     <div className="App">
+      <Header />
       <Posters title='All Movies' movieData={movieData.movies} />
+      <Footer />
     </div>
   );
 }
 
 export default App;
+
+//decide which data set to use for dummy data
+//finish creating structure (header, footer, main)
+//refactor app to class component
+
+//create method that will show new image on click
+  //css class hidden, display none; html element onClick
