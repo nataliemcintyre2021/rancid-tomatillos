ffc1982c1e9123ff92f09787204058be08d1a06f
Matt Roden <mattroden92@gmail.com>
2021-08-27 02:43:55 -0600

    Display and sort data by rating - high to low

--- src/List.js
+++ src/List.js
@@ -2,11 +2,13 @@ import './List.css'
 
 const List = (props) => {
 
-const list = props.movieData.map(movie =>
+let sortedByRating = props.movieData.sort((a, b) => b.average_rating - a.average_rating)
+// console.log(sortedByRating, '<><>')
+const list = sortedByRating.map(movie =>
   <ul>
     <li>
       <p className='list-title'>{movie.title}</p>
-      <p className='list-rating'>{movie.rating}</p>
+      <p className='list-rating'>{movie.average_rating}</p>
     </li>
   </ul>
 )
