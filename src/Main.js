7927e00941f5309d9f09b8c6a31c82d729d4925f
Matt Roden <mattroden92@gmail.com>
2021-08-27 17:42:11 -0400

    Add handlePotserClick function - trying to pass clicked poster ID down

--- src/Main.js
+++ src/Main.js
@@ -10,7 +10,8 @@ class Main extends React.Component {
     super();
     this.state = {
       movieData: movieData.movies,
-      isExtendedView: false
+      isExtendedView: false,
+      clickedPosterID: null
     }
   }
 
@@ -23,11 +24,15 @@ class Main extends React.Component {
     }
   }
 
+  handlePosterClick = (event) => {
+    this.setState({clickedPosterID: event.target.id})
+  }
+
   render() {
     return !this.state.isExtendedView ? (
       <main className='main-section'>
         <div className='all-movies-container'>
-          <Posters title='All Movies' movieData={this.state.movieData} changeExtendedState={this.changeExtendedState} />
+          <Posters title='All Movies' movieData={this.state.movieData} changeExtendedState={this.changeExtendedState} onClick={event => this.handlePosterClick(event)} />
           <Posters title='More Movies' movieData={this.state.movieData} changeExtendedState={this.changeExtendedState} />
           <Posters title='Even More Movies' movieData={this.state.movieData} changeExtendedState={this.changeExtendedState} />
         </div>
@@ -38,7 +43,7 @@ class Main extends React.Component {
         <div className='single-movie-container'>
           <ExtendedView
             singleMovieData={this.state.movieData}
-            id={'337401'}
+            id='726739'
             changeExtendedState={this.changeExtendedState}
             />
         </div>
