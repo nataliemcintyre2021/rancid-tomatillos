d856467d59a0d21ff520208a76e145d344adbcb3
Matt Roden <mattroden92@gmail.com>
2021-08-29 20:25:36 -0400

    Style error message - give top margin

--- src/Error.js
+++ src/Error.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import './Error.css'
 
 function Error(props) {
   console.log(error.message)
   return (
     <section>
-      <h1>Oops! Looks like something went wrong with the server. Please refresh the page or try again later</h1>
+      <h1 className='error-message'>Oops! Looks like something went wrong with the server. Please refresh the page or try again later</h1>
     </section>
   )
 }
