7d637a0a93f523cc57b0aa4c0eeec4b87f33c814
Matt Roden <mattroden92@gmail.com>
2021-08-29 20:22:55 -0400

    Add error component and css file - initial build of error comp

---
+++ src/Error.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Error(props) {
+  console.log(error.message)
+  return (
+    <section>
+    <h1>Oops! Looks like something went wrong with the server. Please refresh the page or try again later</h1>
+    </section>
+  )
+}
+
+export default Error;
