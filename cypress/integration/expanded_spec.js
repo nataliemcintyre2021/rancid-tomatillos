01426ce10e2a1dc251cf9abcb91349ce6792cfd8
Natalie McIntyre <nataliemcintyre2021@gmail.com>
2021-08-30 14:27:18 -0700

    Add beforeEach to test file

---
+++ cypress/integration/expanded_spec.js
@@ -0,0 +1,9 @@
+beforeEach(() => {
+  cy.visit('http://localhost:3000/dashboard');
+});
+
+describe('Expanded single movie poster view flow', () => {
+  it('Should be able to click on movie poster and see expanded view of single movie', () => {
+
+  })
+})
