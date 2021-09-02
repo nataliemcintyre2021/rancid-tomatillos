2e4a9778c13910b1ba83954b688eddcc3e073d8f
Matt Roden <mattroden92@gmail.com>
2021-09-01 22:35:51 -0600

    Rename classes to fit component name change from Row to Posters

--- src/Posters.js
+++ src/Posters.js
@@ -11,9 +11,9 @@ function Posters(props) {
   )
 
   return (
-    <div className='row-container'>
-      <h2 className='row-title'>{props.title}</h2>
-      <div className="row-posters">
+    <div className='posters-container'>
+      <h2 className='posters-title'>{props.title}</h2>
+      <div className="row-of-posters">
         {movies}
       </div>
     </div>
