6114c726881e36eafb72b2ed0f244e49de7c302f
Matt Roden <mattroden92@gmail.com>
2021-09-04 13:09:04 -0600

    Update tests to be more robust

--- cypress/integration/main_spec.js
+++ cypress/integration/main_spec.js
@@ -1,15 +1,22 @@
 beforeEach(() => {
-  cy.visit('http://localhost:3000')
+  cy.visit('http://localhost:3000/movies')
 })
 
 describe('Main movie cards display of App', () => {
 
-  it('Should confirm that true is equal to be true', () => {
+  it('Should confirm that true is equal to true', () => {
     expect(true).to.equal(true)
   })
 
+  it('Should show a loading message while the movie data is being fetched', () => {
+    cy.get('p').contains('Loading...')
+  })
+
   it('Should display the correct elements on page load', () => {
-    cy.get('main').find('div')
+    cy.get('header').find('nav').contains('Nav goes here')
+    cy.get('main div:first').find('h2').contains('All Movies')
+    cy.get('main').find('.row-of-posters')
+    cy.get('ul').find('li').should('have.length', 40)
 
   });
 
@@ -19,7 +26,6 @@ describe('Main movie cards display of App', () => {
 
   it('Should fecth data for the 40 movies cards', () => {
     cy.intercept('https://rancid-tomatillos.herokuapp.com/api/v2/movies', {fixtures: 'movies.json'}).as('moviesRoute')
-    cy.visit('http://localhost:3000')
     cy.wait('@moviesRoute').then((interception) => {
       'response.ok'
     })
