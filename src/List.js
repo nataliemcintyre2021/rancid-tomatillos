d3c0ff9766cf6c1ee577b12cb163804aec512ca3
Matt Roden <mattroden92@gmail.com>
2021-09-04 16:14:15 -0600

    Convert List to class based component

--- src/List.js
+++ src/List.js
@@ -1,26 +1,40 @@
+import React from 'react'
 import './List.css'
 
-const List = (props) => {
+class List extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      listData: [],
+      error: ''
+    }
+  }
 
-let sortedByRating = props.listData.sort((a, b) => b.average_rating - a.average_rating)
-// console.log(sortedByRating, '<><>')
-const list = sortedByRating.map(movie =>
-
-    <li>
-      <p className='list-title'>{movie.title}</p>
-      <p className='list-rating'>{(movie.average_rating.toFixed(1) * 10) + '%'}</p>
-    </li>
-
-)
-
-  return (
-    <div className='list-wrapper'>
-      <h2>Movies by Ratings 📈</h2>
-      <ul className='list'>
-        { list }
-      </ul>
-    </div>
-  )
+  
 }
 
 export default List
+
+
+// const List = (props) => {
+//
+// let sortedByRating = props.listData.sort((a, b) => b.average_rating - a.average_rating)
+// // console.log(sortedByRating, '<><>')
+// const list = sortedByRating.map(movie =>
+//
+//     <li>
+//       <p className='list-title'>{movie.title}</p>
+//       <p className='list-rating'>{(movie.average_rating.toFixed(1) * 10) + '%'}</p>
+//     </li>
+//
+// )
+//
+//   return (
+//     <div className='list-wrapper'>
+//       <h2>Movies by Ratings 📈</h2>
+//       <ul className='list'>
+//         { list }
+//       </ul>
+//     </div>
+//   )
+// }
