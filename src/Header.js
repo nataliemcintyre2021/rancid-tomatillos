534649b070726d0b9f0a8375c4a80114ea1052ed
Matt Roden <mattroden92@gmail.com>
2021-09-05 17:02:50 -0600

    Go back to old header style

--- src/Header.js
+++ src/Header.js
@@ -1,24 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import './Header.css'
 
 function Header() {
-  const [show, handleShow] = useState(false)
-
-  useEffect(() => {
-      window.addEventListener('scroll', () => {
-        if (window.scrollY > 100) {
-          handleShow(true)
-        } else {
-          handleShow(false)
-        }
-      })
-      return () => {
-        window.removeEventListener('scroll')
-      }
-    }, [])
-
     return (
-      <header className={`header ${show && 'header_red'}`}>
+      <header className='header'>
         <nav>Nav goes here</nav>
       </header>
     )
