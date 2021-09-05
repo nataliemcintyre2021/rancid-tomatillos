cc4e9178a1602b90027fef671e09b1ad35a41f7a
Matt Roden <mattroden92@gmail.com>
2021-09-05 17:44:40 -0600

    Add target blank to links in footer

--- src/Footer.js
+++ src/Footer.js
@@ -6,8 +6,16 @@ function Footer() {
     <footer className='footer'>
       <h4>Project Developers:</h4>
       <div className='link-container'>
-        <a href='https://github.com/Matt-Roden' className='footer-link'>Matt Roden</a>
-        <a href='https://github.com/nataliemcintyre2021' className='footer-link'>Natalie McIntyre</a>
+        <a
+          href='https://github.com/Matt-Roden'
+          className='footer-link'
+          target='_blank'
+          >Matt Roden</a>
+        <a
+          href='https://github.com/nataliemcintyre2021'
+          className='footer-link'
+          target='_blank'
+          >Natalie McIntyre</a>
       </div>
 
     </footer>
