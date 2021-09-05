2ef0522a26388f4e2b81775ee90e25b44005c88a
Matt Roden <mattroden92@gmail.com>
2021-09-05 17:42:56 -0600

    Restyle footer to include devs github links

--- src/Footer.js
+++ src/Footer.js
@@ -3,7 +3,14 @@ import './Footer.css'
 
 function Footer() {
   return (
-    <footer className='footer'>Footer goes here</footer>
+    <footer className='footer'>
+      <h4>Project Developers:</h4>
+      <div className='link-container'>
+        <a href='https://github.com/Matt-Roden' className='footer-link'>Matt Roden</a>
+        <a href='https://github.com/nataliemcintyre2021' className='footer-link'>Natalie McIntyre</a>
+      </div>
+
+    </footer>
   )
 }
 
