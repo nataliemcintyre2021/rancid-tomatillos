942ac76c598470fbd0bc7d1b736622e338552984
Natalie McIntyre <nataliemcintyre2021@gmail.com>
2021-09-04 17:17:38 -0700

    Update fixtures file

--- cypress/integration/expanded_spec.js
+++ cypress/integration/expanded_spec.js
@@ -1,11 +1,13 @@
-beforeEach(() => {
-  cy.visit('http://localhost:3000/')
-  // cy.fixture('../fixtures/singleMovies.json')
-  // cy.intercept("GET", 'http://localhost:3000/dashboard', {fixture: })
-});
+// beforeEach(() => {
+//   cy.visit('http://localhost:3000/')
+//   // cy.fixture('../fixtures/singleMovies.json')
+//   // cy.intercept("GET", 'http://localhost:3000/dashboard', {fixture: })
+// });
 
 describe('Expanded single movie poster view flow', () => {
-
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/')
+  })
   it('Should confirm true to be true', () => {
     expect(true).to.equal(true)
   })
@@ -15,18 +17,94 @@ describe('Expanded single movie poster view flow', () => {
     cy.url().should('include', '/694919')
   })
 
-  it('Should fecth data for the 2 stubbed movies cards and render the correpsonding elements', () => {
-    cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies', { fixture: 'moneyplane'}).as('moneyplane')
-    cy.visit('http://localhost:3000/movies')
+  it('Should render a header', () => {
+    cy.get('.single-poster').first().click()
+    cy.get('header')
+      .contains('Nav goes here')
+  })
+
+  it('Should render a footer', () => {
+    cy.get('.single-poster').first().click()
+    cy.get('footer')
+      .contains('Footer goes here')
+  })
+
+  it('Should fetch data for first movie card and render the correpsonding elements', () => {
+    cy.visit('http://localhost:3000/694919')
+    cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies/694919', { fixture: 'moneyplane'}).as('moneyplane')
     cy.wait('@moneyplane').then((interception) => {
       'response.ok'
     })
-    cy.get('main div:first').find('h2').contains('All Movies')
-    cy.get('main').find('a').should('have.class', 'single-poster')
+
+      .get(".movie-image")
+        .should('have.attr', 'style')
+        .and('equal', 'background-image: url("https://image.tmdb.org/t/p/original//pq0JSpwyT2URytdFG0euztQPAyR.jpg"); background-size: cover; height: 75vh;')
+
+      .get(".title")
+        .contains('Money Plane')
+
+      .get(".release-date")
+        .contains('2020-09-29')
+
+      .get(".genre")
+        .contains('Action')
+
+       .get(".runtime")
+          .contains("82 minutes")
+
+       // .get(".tagline")
+       //    .contains("")
+
+       .get(".overview")
+         .contains("A professional thief with $40 million in debt and his family's life on the line must commit one final heist - rob a futuristic airborne casino filled with the world's most dangerous criminals.")
 
   })
 
+  // it('Should fetch data for the 2 stubbed movies cards and render the correpsonding elements', () => {
+  //   cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies/694919', { fixture: 'moneyplane'}).as('moneyplane')
+  //   cy.wait('@moneyplane').then((interception) => {
+  //     'response.ok'
+  //   })
+  //   cy.visit('http://localhost:3000/694919')
+  //   cy.get('main div:first').find('h2').contains('All Movies')
+  //   cy.get('main').find('a').should('have.class', 'single-poster')
+  //
+  // })
+
 })// End describe block
+
+// describe('Intercepting and stubbing fetched movie data', () => {
+//   it('Should fetch data for first movie card and render the correpsonding elements', () => {
+//     cy.visit('http://localhost:3000/694919')
+//     cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies/694919', { fixture: 'moneyplane'}).as('moneyplane')
+//     cy.wait('@moneyplane').then((interception) => {
+//       'response.ok'
+//     })
+//
+//       .get(".movie-image")
+//         .should('have.attr', 'style')
+//         .and('equal', 'background-image: url("https://image.tmdb.org/t/p/original//pq0JSpwyT2URytdFG0euztQPAyR.jpg"); background-size: cover; height: 75vh;')
+//
+//       .get(".title")
+//         .contains('Money Plane')
+//
+//       .get(".release-date")
+//         .contains('2020-09-29')
+//
+//       .get(".genre")
+//         .contains('Action')
+//
+//        .get(".runtime")
+//           .contains("82 minutes")
+//
+//        // .get(".tagline")
+//        //    .contains("")
+//
+//        .get(".overview")
+//          .contains("A professional thief with $40 million in debt and his family's life on the line must commit one final heist - rob a futuristic airborne casino filled with the world's most dangerous criminals.")
+//
+//   })
+// })
 
 
 
@@ -34,15 +112,15 @@ describe('Expanded single movie poster view flow', () => {
 //it should render a header
 //it should render a footer
 //it should show a loading message while the movie data is being fetched
-//it should fetch data for the 2 stubbed movie cards and render corresponding elements
-//it should fetch data for the 2 stubbed movie cards and show movie posters
-//it should fetch data for the 2 stubbed movie cards and show movie titles
-//it should fetch data for the 2 stubbed movie cards and show movie ratings
-//it should fetch data for the 2 stubbed movie cards and show movie release dates
-//it should fetch data for the 2 stubbed movie cards and show movie genres
-//it should fetch data for the 2 stubbed movie cards and show movie runtimes
-//it should fetch data for the 2 stubbed movie cards and show movie overviews
-//it should fetch data for the 2 stubbed movie cards and show movie tag lines
+//it should fetch data for the movie card and render corresponding elements
+//it should fetch data for the stubbed movie card and show movie posters
+//it should fetch data for the stubbed movie card and show movie titles
+//it should fetch data for the stubbed movie card and show movie ratings
+//it should fetch data for the stubbed movie card and show movie release dates
+//it should fetch data for the stubbed movie card and show movie genres
+//it should fetch data for the stubbed movie card and show movie runtimes
+//it should fetch data for the stubbed movie card and show movie overviews
+//it should fetch data for the stubbed movie card and show movie tag lines
 
 
 
