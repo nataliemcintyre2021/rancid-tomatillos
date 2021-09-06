1773a9f7f1292af5a6b19a773f9367d5ef9e892d
Matt Roden <mattroden92@gmail.com>
2021-09-06 13:59:41 -0600

    Clean up - add semi-colons

--- cypress/integration/main_spec.js
+++ cypress/integration/main_spec.js
@@ -6,25 +6,25 @@ describe('Main movie cards display of App', () => {
 
   it('Should confirm that true is equal to true', () => {
     expect(true).to.equal(true)
-  })
+  });
 
   it('Should show a loading message while the movie data is being fetched', () => {
     cy.get('p').contains('Loading... 😎')
-  })
+  });
 
   it('Should render a header component', () => {
     cy.get('header').find('nav').contains('Rancid Tomatillos')
-  })
+  });
 
   it('Should render a search bar input and a label', () => {
     cy.get('input')
     cy.get('label').contains('Search By Title')
-  })
+  });
 
   it('Should render a component that displays all the movie cards', () => {
     cy.get('.posters-container').find('h2').contains('All Movies')
     cy.get('main').find('.row-of-posters')
-  })
+  });
 
   it('Should display 40 movie cards', () => {
     cy.get('main').find('a').should('have.length', 40)
@@ -33,7 +33,7 @@ describe('Main movie cards display of App', () => {
   it('Should render a list that ranks movies from highest to lowest rating', () => {
     cy.get('.list-wrapper').find('h2').contains('Movies by Ratings')
     cy.get('ul').find('li').should('have.length', 40)
-  })
+  });
 
   it('Should render a footer component', () => {
     cy.get('footer').contains('Project Developers:')
@@ -49,7 +49,7 @@ describe('Main movie cards display of App', () => {
     cy.get('main').find('a').should('have.length', 2)
     cy.get('.list-wrapper').find('h2').contains('Movies by Ratings')
     cy.get('ul').find('li').should('have.length', 2)
-  })
+  });
 
   it('Should be able to search for movies by title', () => {
     cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies', { fixture: 'movies'}).as('movies')
@@ -59,7 +59,7 @@ describe('Main movie cards display of App', () => {
     })
     cy.get('input').type('Mon')
     cy.get('main').find('a').should('have.length', 1)
-  })
+  });
 
   it('Should be able to visit the found movie\'s info page', () => {
     cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies', { fixture: 'movies'}).as('movies')
@@ -70,7 +70,6 @@ describe('Main movie cards display of App', () => {
     cy.get('input').type('Mon')
     cy.get('main').find('a').click()
     cy.url().should('include', '/694919')
-  })
+  });
 
-
-})// End describe block
+})
